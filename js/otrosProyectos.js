282d12436bcca166c3f28c90c44f7520941bffa7
Sergioba99 <64738043+Sergioba99@users.noreply.github.com>
2026-04-07 19:14:36 +0200

    Update otrosProyectos.js

--- js/otrosProyectos.js
+++ js/otrosProyectos.js
@@ -12,7 +12,6 @@
     {name: "Fan Control", sub: "ESP32 · Arduino", file: "projects/fanControl.html"},
   ];
 
-  
   let currentIndex = 0;
   const viewer    = document.getElementById('op-viewer');
   const sideList  = document.getElementById('op-sidebar-list');
